9b8b3f6aa4f8843682f1ab47b38161f3e69cb804
SangIkCheon <crackers0424@gmail.com>
2022-08-30 21:38:44 +0900

    초기설정

---
+++ src/main/java/com/ik/market/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ik.market.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("Hello");
+        return "Hello #1";
+    }
+}
